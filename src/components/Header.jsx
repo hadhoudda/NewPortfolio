7256b069dab3fc24dff1f7149cffe1e8c0571974
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-11-01 22:16:55 +0100

    creat header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import Navbar from "./Navbar";
-import logo from "../assets/logo-houda.png";
-import "../styles/header.sass";
+// import logo from "../../public/logo.png";
+import "../styles/header.scss";
 
 function Header() {
     return (
         <header>
-            <Link to='/Portfolio'><img className ="logo" src={logo} alt='image__houda' /></Link>
+            <p className="text-name">Houda Ben Abdallah</p>
+            {/* <Link to='/Portfolio'><img className ="logo" src={logo} alt='image__houda' /></Link> */}
             <Navbar/>
         </header>
     )
