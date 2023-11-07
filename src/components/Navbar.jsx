d76f6e874a6fe4c6005271420106d4e214161658
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-11-07 22:40:17 +0100

    fin header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.scss'
 
 function Navbar() {
     const [toggleMenu, setToggleMenu ] = useState (false);
-    const [largeur, setLargeur] = useState(window.innerWidth)
 
     const toggleNavSmallScreen =() => {
         setToggleMenu (!toggleMenu);
+
     }
 
-    useEffect (() => {
-        const changeWidth = () => {
-            setLargeur(window.innerWidth);
-            if(window.innerWidth>680){
-                setToggleMenu(false)
-            }
-        }
-        window.addEventListener('resize', changeWidth);
-        return () => {
-            window.removeEventListener('resize', changeWidth);
-        }
-    }, [])
-    
   return (
     <nav className='nav-responsive'>
-        <ul className="liste">
-            <Link to = '/Portfolio'><li className="items">Accueil </li></Link>
-            <Link to = '/Portfolio/profil'><li className="items">A propos </li></Link>
-            <Link to = '/Portfolio/competance'><li className="items">Compétences </li></Link>
-            <Link to = '/Portfolio/portfolio'><li className="items">Projets</li></Link>
-            <Link to = '/Portfolio/contact'><li className="items">Contact </li></Link>
+        <ul className={toggleMenu ? "liste": "liste-responsive" }>
+            <li className="items"><Link to = '/Portfolio'>Accueil </Link></li>
+            <li className="items"><Link to = '/Portfolio/profil'>A propos </Link></li>
+            <li className="items"><Link to = '/Portfolio/competance'>Compétences </Link></li>
+            <li className="items"><Link to = '/Portfolio/portfolio'>Projets</Link></li>
+            <li className="items"><Link to = '/Portfolio/contact'>Contact </Link></li>
         </ul>
-        {toggleMenu && ( //si toggleMenu true envoi liste si false rien de tout
-            <ul className=" liste-responsive">
-                <Link to = '/Portfolio' className="link-items"><li onClick={toggleNavSmallScreen} className="items" >Accueil </li></Link>
-                <Link to = '/Portfolio/profil' className="link-items"><li onClick={toggleNavSmallScreen} className="items">A propos </li></Link>
-                <Link to = '/Portfolio/competance' className="link-items"><li onClick={toggleNavSmallScreen} className="items">Compétences </li></Link>
-                <Link to = '/Portfolio/portfolio' className="link-items"><li onClick={toggleNavSmallScreen} className="items">Projets</li></Link>
-                <Link to = '/Portfolio/contact' className="link-items"><li onClick={toggleNavSmallScreen} className="items">Contact </li></Link>
-            </ul>            
-        )}
-        <div onClick={toggleNavSmallScreen} ><i className="fa-solid fa-bars btn"></i></div>
+        <div onClick={toggleNavSmallScreen} ><i className={toggleMenu  ? "fa-solid fa-bars btn" :"fa-solid fa-xmark btn "  }></i> </div>
     </nav>
   )
 }
